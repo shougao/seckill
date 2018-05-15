d8491e75ea982aadb9b695e2eb97ed5dbd713d9e
小柒2012 <345849402@qq.com>
2018-05-15 19:18:53 +0800

    启动配置说明

--- src/main/java/com/itstyle/seckill/Application.java
+++ src/main/java/com/itstyle/seckill/Application.java
@@ -16,6 +16,7 @@ public class Application {
 	/**
 	 * 1. 数据库乐观锁；2. 基于Redis的分布式锁；3. 基于ZooKeeper的分布式锁
 	 * 4. redis 订阅监听；5.kafka消息队列
+	 * 启动前 请配置application.properties中相关redis、zk以及kafka相关地址
 	 */
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(Application.class, args);
