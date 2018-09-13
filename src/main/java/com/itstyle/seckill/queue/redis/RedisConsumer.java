fe41832c3bc601300a4fcef1320c785a1e50e0f2
小柒2012 <345849402@qq.com>
2018-09-13 11:00:43 +0800

    Redis和kafka消费队列逻辑判断问题修复

--- src/main/java/com/itstyle/seckill/queue/redis/RedisConsumer.java
+++ src/main/java/com/itstyle/seckill/queue/redis/RedisConsumer.java
@@ -23,7 +23,7 @@ public class RedisConsumer {
     public void receiveMessage(String message) {
         //收到通道的消息之后执行秒杀操作(超卖)
     	String[] array = message.split(";"); 
-    	if(redisUtil.getValue(array[0])!=null){//control层已经判断了，其实这里不需要再判断了
+    	if(redisUtil.getValue(array[0])==null){//control层已经判断了，其实这里不需要再判断了
     		Result result = seckillService.startSeckilDBPCC_TWO(Long.parseLong(array[0]), Long.parseLong(array[1]));
     		if(result.equals(Result.ok(SeckillStatEnum.SUCCESS))){
     			WebSocketServer.sendInfo(array[0].toString(), "秒杀成功");//推送给前台
