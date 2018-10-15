1da07fd81f8e0a33379e2851f6fd7df03daaa7d9
小柒2012 <345849402@qq.com>
2018-10-15 14:46:37 +0800

    Actuator健康监控

---
+++ src/main/java/com/itstyle/seckill/web/RedPacketController.java
@@ -0,0 +1,12 @@
+package com.itstyle.seckill.web;
+
+import io.swagger.annotations.Api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Api(tags ="抢红包")
+@RestController
+@RequestMapping("/redPacket")
+public class RedPacketController {
+	
+}
